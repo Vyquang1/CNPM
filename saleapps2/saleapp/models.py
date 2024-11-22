142ea2527be536e271268000e7585a68e0a18903
Vyquang1 <2254050084vy@ou.edu.vn>
2024-11-22 16:05:00 +0700

    22/11

---
+++ saleapps2/saleapp/models.py
@@ -0,0 +1,41 @@
+from  sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from saleapp import app, db
+
+
+class Category(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    products=relationship('Product',backref="category",lazy=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(db.Model):
+    id=Column(Integer,primary_key=True, autoincrement=True)
+    name=Column(String(50),unique=True,nullable=False)
+    price=Column(Float,default=0)
+    image=Column(String(200),default="https://res.cloudinary.com/dy1unykph/image/upload/v1729842193/iPhone_15_Pro_Natural_1_ltf9vr.webp")
+    category_id=Column(Integer,ForeignKey(Category.id),nullable=False)
+    active=Column(Boolean,default=True)
+
+    def __str__(self):
+        return self.name
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        c1 = Category(name="Mobile")
+        c2 = Category(name="Tablet")
+        c3 = Category(name="Laptop")
+        db.session.add_all([c1,c2,c3])
+
+
+        import json
+        with open('data/products.json', encoding='utf-8') as f:
+            products = json.load(f)
+            for p in products:
+                prod=Product(**p)
+                db.session.add(prod)
+
+        db.session.commit()
